4e6789b66bc93e1d37c69abb6b904b7db4989a59
Jiwon <126679650+z1one516@users.noreply.github.com>
2023-07-05 13:55:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 47.25 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,17 +1,9 @@
 def solution(participant, completion):
-    temp = {} # key,value를 가진 딕셔너리 이용
-    #딕셔너리에 참가자 이름(key)과 해당 이름 사람수(value) 넣기 
-    for i in participant :
-        if i in temp : 
-            temp[i]+=1
-        else: 
-            temp[i] =1
-    
-    for i in completion :
-        if temp[i]==1 : 
-            del temp[i]
-        else : 
-            temp[i] -=1
-
-    #딕셔너리를 리스트로 바꾸고 가장 첫번째꺼 리턴(어차피 하나뿐)
-    return list(temp.keys())[0]
+    people_dict = { i: participant.count(i) for i in participant}
+    for i in completion:
+        if people_dict[i] == 1:
+            del people_dict[i]
+        else:
+            people_dict[i] -= 1
+    answer = list(people_dict.keys())[0]
+    return answer 
